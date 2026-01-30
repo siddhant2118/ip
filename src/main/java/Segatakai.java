fda69b0f89466797fa9702b503a9fdd88aee3129
Singh Siddhant Narayan <e1375752@u.nus.edu>
2026-01-30 15:01:46 +0800

    Level 3: Mark, Unmark

--- src/main/java/Segatakai.java
+++ src/main/java/Segatakai.java
@@ -5,7 +5,7 @@ public class Segatakai {
         Scanner scanner = new Scanner(System.in);
 
         //Storage for tasks
-        String[] tasks = new String[100];  //Array to store tasks
+        Task[] tasks = new Task[100];  //Array to store tasks
         int taskCount = 0;                 // Counter for number of tasks
 
         //Greeting
@@ -24,14 +24,31 @@ public class Segatakai {
             else if (input.equals("list")){
                 //Display the tasks
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i=0; i<taskCount; i++){
-                    System.out.println(" " + (i+1) + ". " + tasks[i]);
+                    System.out.println(" " + (i+1) + "." + tasks[i]);
                 }
+                System.out.println("____________________________________________________________");
+            }
+            else if (input.startsWith("mark ")){
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                tasks[index].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[index]);
+                System.out.println("____________________________________________________________");
+            }
+            else if (input.startsWith("unmark ")){
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                tasks[index].markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[index]);
                 System.out.println("____________________________________________________________");
             }
             else{
                 //Add task to the list
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
 
                 System.out.println("____________________________________________________________");
