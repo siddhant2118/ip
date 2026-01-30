9486eb1a6e01eb7143005809287a34dfd8d7d69b
Singh Siddhant Narayan <e1375752@u.nus.edu>
2026-01-30 15:05:07 +0800

    A-CodingStandard: Apply Java coding standard

--- src/main/java/Segatakai.java
+++ src/main/java/Segatakai.java
@@ -4,50 +4,46 @@ public class Segatakai {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //Storage for tasks
-        Task[] tasks = new Task[100];  //Array to store tasks
-        int taskCount = 0;                 // Counter for number of tasks
+        // Storage for tasks
+        Task[] tasks = new Task[100];  // Array to store tasks
+        int taskCount = 0;             // Counter for number of tasks
 
-        //Greeting
+        // Greeting
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm SEGATAKAI");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        //Loop to read commands
-        while(true){
+        // Loop to read commands
+        while (true) {
             String input = scanner.nextLine();
 
             if (input.equals("bye")) {
                 break;
-            }
-            else if (input.equals("list")){
-                //Display the tasks
+            } else if (input.equals("list")) {
+                // Display the tasks
                 System.out.println("____________________________________________________________");
                 System.out.println(" Here are the tasks in your list:");
-                for (int i=0; i<taskCount; i++){
-                    System.out.println(" " + (i+1) + "." + tasks[i]);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
-            }
-            else if (input.startsWith("mark ")){
+            } else if (input.startsWith("mark ")) {
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 tasks[index].markAsDone();
                 System.out.println("____________________________________________________________");
                 System.out.println(" Nice! I've marked this task as done:");
                 System.out.println("   " + tasks[index]);
                 System.out.println("____________________________________________________________");
-            }
-            else if (input.startsWith("unmark ")){
+            } else if (input.startsWith("unmark ")) {
                 int index = Integer.parseInt(input.substring(7)) - 1;
                 tasks[index].markAsNotDone();
                 System.out.println("____________________________________________________________");
                 System.out.println(" OK, I've marked this task as not done yet:");
                 System.out.println("   " + tasks[index]);
                 System.out.println("____________________________________________________________");
-            }
-            else{
-                //Add task to the list
+            } else {
+                // Add task to the list
                 tasks[taskCount] = new Task(input);
                 taskCount++;
 
@@ -56,7 +52,7 @@ public class Segatakai {
                 System.out.println("____________________________________________________________");
             }
         }
-        //Goodbye message
+        // Goodbye message
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,25 +2,25 @@ public class Task {
     private String description;
     private boolean isDone;
 
-    public Task(String description){
+    public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
-    public void markAsDone(){
+    public void markAsDone() {
         this.isDone = true;
     }
 
-    public void markAsNotDone(){
+    public void markAsNotDone() {
         this.isDone = false;
     }
 
-    public String getStatusIcon(){
+    public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
 }
