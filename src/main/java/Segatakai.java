acf877d57fb377c055aa355872bfed60039b59b7
Singh Siddhant Narayan <e1375752@u.nus.edu>
2026-01-30 14:35:05 +0800

    Level-1: Echo

--- src/main/java/Segatakai.java
+++ src/main/java/Segatakai.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Segatakai {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Greeting
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm SEGATAKAI");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        //Loop to read commands
+        while(true){
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            //Echo the input back
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+        //Goodbye message
+        System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+
+        scanner.close();
     }
 }
